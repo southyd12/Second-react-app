7252880d137112c2369113c2361e538637681b9b
southyd12 <southd12@gmail.com>
2023-03-06 14:52:07 +0000

    router and layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from './components/Layout';
+
+import List from './pages/List';
+import Add from './pages/Add';
+import Update from './pages/Update';
+import NotFound from './pages/NotFound';
+
 
 function App() {
 
   return (
-    
+    <Router>
+    {/* <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <UIProvider>
+        <CarsProvider> */}
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<List />}/>
+              <Route path="/add" element={<Add />}/>
+              <Route path="/update/:id" element={<Update />}/>
+              <Route path="*" element={<NotFound />}/>
+            </Route>
+          </Routes>        
+        {/* </CarsProvider>
+      </UIProvider>
+    </ThemeProvider> */}
+      
+    </Router>
   )
 }
 
