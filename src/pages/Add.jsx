11a42b0927c1a2f0f7b33a72d2a929e45601f185
southyd12 <southd12@gmail.com>
2023-03-08 16:36:24 +0000

    update and add functionality added

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { useNavigate } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import CartoonForm from '../components/forms/CartoonForm';
+import { CartoonsContext } from '../components/contexts/cartoon.context';
 
 function Add() {
+  const {addCartoon} = useContext(CartoonsContext)
+  const navigate = useNavigate();
+
+  const submitHandler = (data) => {
+    addCartoon(data);
+    navigate('/');
+  }
+
   return (
     <>
       <Typography
          variant="h2"
          component="h1"      
         >
-        Add Car              
+        Add Cartoon              
       </Typography>
       <CartoonForm submitHandler={submitHandler} />
     </>
--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { useParams } from 'react-router-dom'
 import Typography from '@mui/material/Typography';
 import CartoonForm from '../components/forms/CartoonForm';
+import { CartoonsContext } from '../components/contexts/cartoon.context';
 
 function Update() {
   const {id} = useParams();
+  const {cartoons, updateCartoon} = useContext(CartoonsContext);
+
+  const cartoon = cartoons.find(({_id}) => id === _id);
+
   return (
     <>
       <Typography
          variant="h2"
-         component="h1"      
+         component="h1"
+         sx={{marginBottom: 2}}       
         >
         Update Cartoon              
       </Typography>
-      <CartoonForm submitHandler={submitHandler} />
+      <CartoonForm cartoon={cartoon} submitHandler={updateCartoon} />
     </>
   )
 }
