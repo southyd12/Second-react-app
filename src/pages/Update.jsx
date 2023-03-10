e65cf14b3e00ff6f3979f1ce9faa84389b8b9e54
southyd12 <southd12@gmail.com>
2023-03-10 17:38:40 +0000

    sorting bugs

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import DriverForm from '../components/forms/DriverForm';
 import { DriversContext } from '../components/contexts/driver.context';
@@ -7,15 +8,21 @@ import { DriversContext } from '../components/contexts/driver.context';
 function Update() {
   const { id } = useParams();
   const { drivers, updateDriver } = useContext(DriversContext);
+  const navigate = useNavigate();
 
   const driver = drivers.find(({ _id }) => id === _id);
+
+  const updateHandler = (data) => {
+    updateDriver(data);
+    navigate('/');
+  };
 
   return (
     <>
       <Typography variant="h2" component="h1" sx={{display:'flex', justifyContent: 'center', mb: 2}}>
         Update Driver
       </Typography>
-      <DriverForm driver={driver} submitHandler={updateDriver} />
+      <DriverForm driver={driver} submitHandler={updateHandler} />
     </>
   );
 }
